a134c35d730c3fc073cf95e189c7dc854f4ce644
CooperHerlihy <cooper.herlihy@gmail.com>
2025-08-27 11:06:59 -0400

    wrote asm-like interpreter

--- src/viol.c
+++ src/viol.c
@@ -1,85 +1,24 @@
 #include "utils.h"
 
-static constexpr usize HeapSize = 256 * 1024 * 1024;
-static constexpr usize StackSize = 16 * 1024 * 1024;
-
-static constexpr usize FunctionStackSize = 1024;
-static constexpr usize VariableStackSize = 1024;
-static constexpr usize MaxLiteralLength = 256;
-
-enum BuiltinFunction : u8 {
-    BuiltinFunctionPlus,
-    BuiltinFunctionMinus,
-    BuiltinFunctionMultiply,
-    BuiltinFunctionDivide
-};
-
-struct FunctionStack {
-    enum BuiltinFunction *data;
-    enum BuiltinFunction *head;
-    usize size;
-};
-
-[[nodiscard]] static struct FunctionStack create_function_stack(usize size) {
-    struct FunctionStack stack = {
-        .data = halloc(sizeof(enum BuiltinFunction) * size),
-        .size = sizeof(enum BuiltinFunction) * size,
-        .head = stack.data,
-    };
-    return stack;
+[[nodiscard]] static bool is_numeral(int character) {
+    return character >= '0' && character <= '9';
 }
 
-static void push_function(struct FunctionStack *stack, enum BuiltinFunction value) {
-    if (stack->head + 1 > stack->data + stack->size)
-        ERROR("Function stack overflow");
-    *stack->head = value;
-    ++stack->head;
+[[nodiscard]] static bool is_whitespace(int character) {
+    return character == ' ' || character == '\t' || character == '\n' || character == EOF;
 }
 
-[[nodiscard]] static enum BuiltinFunction pop_function(struct FunctionStack *stack) {
-    if (stack->head <= stack->data)
-        ERROR("Function stack underflow");
-    --stack->head;
-    return *stack->head;
-}
+static constexpr usize MaxStringLength = 256;
+static constexpr usize StackSize = 1024;
 
-struct VariableStack {
-    i32 *data;
-    i32 *head;
-    usize size;
-};
-
-[[nodiscard]] static struct VariableStack create_variable_stack(usize size) {
-    struct VariableStack stack = {
-        .data = halloc(sizeof(i32) * size),
-        .size = sizeof(i32) * size,
-        .head = stack.data,
-    };
-    return stack;
-}
-
-static void push_variable(struct VariableStack *stack, i32 value) {
-    if (stack->head + 1 > stack->data + stack->size)
-        ERROR("Variable stack overflow");
-    *stack->head = value;
-    ++stack->head;
-}
-
-[[nodiscard]] static i32 pop_variable(struct VariableStack *stack) {
-    if (stack->head <= stack->data)
-        ERROR("Variable stack underflow");
-    --stack->head;
-    return *stack->head;
-}
-
-struct LiteralStack {
+struct StringStack {
     char *data;
     char *head;
     usize size;
 };
 
-[[nodiscard]] static struct LiteralStack create_literal_stack(usize size) {
-    struct LiteralStack stack = {
+[[nodiscard]] static struct StringStack create_string_stack(usize size) {
+    struct StringStack stack = {
         .data = halloc(sizeof(char) * size),
         .size = sizeof(char) * size,
         .head = stack.data,
@@ -87,139 +26,195 @@ struct LiteralStack {
     return stack;
 }
 
-static void push_literal_char(struct LiteralStack *stack, char value) {
+static void push_string(struct StringStack *stack, char value) {
     if (stack->head + 1 > stack->data + stack->size)
-        ERROR("Literal stack overflow");
+        ERROR("String stack overflow");
     *stack->head = value;
     ++stack->head;
 }
 
-[[nodiscard]] static int pop_literal(struct LiteralStack *stack) {
+[[nodiscard]] static char *pop_string(struct StringStack *stack) {
     if (stack->head <= stack->data)
-        ERROR("Literal stack underflow");
-    *stack->head = '\0';
-    long value = atoi(stack->data);
+        ERROR("No string to pop");
+    push_string(stack, '\0');
+    stack->head = stack->data;
+    return stack->data;
+}
+
+struct ValueStack {
+    i32 *data;
+    i32 *head;
+    usize size;
+};
+
+[[nodiscard]] static struct ValueStack create_value_stack(usize size) {
+    struct ValueStack stack = {
+        .data = halloc(sizeof(i32) * size),
+        .size = sizeof(i32) * size,
+        .head = stack.data,
+    };
+    return stack;
+}
+
+static void push_value(struct ValueStack *stack, i32 value) {
+    if (stack->head + 1 > stack->data + stack->size)
+        ERROR("Value stack overflow");
+    *stack->head = value;
+    ++stack->head;
+}
+
+[[nodiscard]] static i32 pop_value(struct ValueStack *stack) {
+    if (stack->head <= stack->data)
+        ERROR("Value stack underflow");
+    --stack->head;
+    return *stack->head;
+}
+
+enum Operation {
+    OperationNone,
+    OperationPush,
+    OperationPop,
+    OperationCall,
+    OperationReturn,
+};
+
+[[nodiscard]] static enum Operation parse_operation(char *string) {
+    if (strcmp(string, "push") == 0)
+        return OperationPush;
+    if (strcmp(string, "pop") == 0)
+        return OperationPop;
+    if (strcmp(string, "call") == 0)
+        return OperationCall;
+    if (strcmp(string, "return") == 0)
+        return OperationReturn;
+    ERRORF("Unknown operation '%s'", string);
+}
+
+[[nodiscard]] static int parse_literal(char *string) {
+    if (!is_numeral(string[0]))
+        ERRORF("Expected number literal, got '%s'", string);
+
+    long value = atoi(string);
+
     if (value > INT_MAX)
         ERROR("Literal too large");
-    stack->head = stack->data;
     return (int)value;
 }
 
-[[nodiscard]] static bool is_whitespace(int character) {
-    return character == ' ' || character == '\t' || character == '\n';
-}
+enum ParseState {
+    ParseComment,
+    ParseOperation,
+    ParseArgument,
+};
 
-static void parse_function(FILE* src, struct FunctionStack *function_stack) {
-    int character;
-    while (is_whitespace(character = fgetc(src))) {}
-    switch (character) {
-        case '+': {
-            push_function(function_stack, BuiltinFunctionPlus);
-        } break;
-        case '-': {
-            push_function(function_stack, BuiltinFunctionMinus);
-        } break;
-        case '*': {
-            push_function(function_stack, BuiltinFunctionMultiply);
-        } break;
-        case '/': {
-            push_function(function_stack, BuiltinFunctionDivide);
-        } break;
-        default: ERRORF("Unexpected character '%c' in function name", character);
+struct ParseData {
+    struct StringStack string_stack;
+    struct ValueStack stack;
+    enum ParseState state;
+    enum Operation operation;
+};
+
+static void call_function(struct ParseData *parse_data, char *name) {
+    if (strcmp(name, "print") == 0) {
+        int value = pop_value(&parse_data->stack);
+        printf("%d\n", value);
+    } else if (strcmp(name, "add") == 0) {
+        int augend = pop_value(&parse_data->stack);
+        int addend = pop_value(&parse_data->stack);
+        push_value(&parse_data->stack, augend + addend);
+    } else if (strcmp(name, "sub") == 0) {
+        int subtrahend = pop_value(&parse_data->stack);
+        int minuend = pop_value(&parse_data->stack);
+        push_value(&parse_data->stack, minuend - subtrahend);
+    } else if (strcmp(name, "mul") == 0) {
+        int multiplier = pop_value(&parse_data->stack);
+        int multiplicand = pop_value(&parse_data->stack);
+        push_value(&parse_data->stack, multiplicand * multiplier);
+    } else if (strcmp(name, "div") == 0) {
+        int divisor = pop_value(&parse_data->stack);
+        if (divisor == 0)
+            ERROR("Division by zero");
+        int numerator = pop_value(&parse_data->stack);
+        push_value(&parse_data->stack, numerator / divisor);
+    } else {
+        ERRORF("Unknown function '%s'", name);
     }
 }
 
-[[nodiscard]] static bool is_numeral(int character) {
-    return character >= '0' && character <= '9';
-}
-
-[[nodiscard]] static i32 parse_literal(FILE* src, struct LiteralStack *literal_stack) {
-    int character;
-    while (is_numeral(character = fgetc(src))) {
-        push_literal_char(literal_stack, (char)character);
-    }
-    fseek(src, -1, SEEK_CUR);
-    return pop_literal(literal_stack);
-}
-
-static void execute_funtion(struct FunctionStack *function_stack, struct VariableStack *variable_stack) {
-    enum BuiltinFunction function = pop_function(function_stack);
-    switch (function) {
-        case BuiltinFunctionPlus: {
-            int augend = pop_variable(variable_stack);
-            int addend = pop_variable(variable_stack);
-            push_variable(variable_stack, augend + addend);
+static void execute_operation(struct ParseData *parse_data, char* arg) {
+    switch (parse_data->operation) {
+        case OperationPush: {
+            push_value(&parse_data->stack, parse_literal(arg));
         } break;
-        case BuiltinFunctionMinus: {
-            int subtrahend = pop_variable(variable_stack);
-            int minuend = pop_variable(variable_stack);
-            push_variable(variable_stack, minuend - subtrahend);
+        case OperationPop: {
+            ASSERT(false);
         } break;
-        case BuiltinFunctionMultiply: {
-            int multiplier = pop_variable(variable_stack);
-            int multiplicand = pop_variable(variable_stack);
-            push_variable(variable_stack, multiplicand * multiplier);
+        case OperationCall: {
+            call_function(parse_data, arg);
         } break;
-        case BuiltinFunctionDivide: {
-            f32 divisor = (f32)pop_variable(variable_stack);
-            if (divisor == 0)
-                ERROR("Division by zero");
-            f32 numerator = (f32)pop_variable(variable_stack);
-            push_variable(variable_stack, (int)(numerator / divisor));
+        case OperationReturn: {
+            ASSERT(false);
         } break;
-        default: ERRORF("Unknown function %d", function);
+        default: {
+            ASSERT(false);
+        } break;
     }
 }
 
-static void parse(FILE* src) {
-    struct FunctionStack function_stack = create_function_stack(FunctionStackSize);
-    struct VariableStack variable_stack = create_variable_stack(VariableStackSize);
-    struct LiteralStack literal_stack = create_literal_stack(MaxLiteralLength);
+void parse(FILE *src) {
+    struct ParseData parse_data = {
+        .string_stack = create_string_stack(MaxStringLength),
+        .stack = create_value_stack(StackSize),
+        .state = ParseOperation,
+        .operation = OperationNone,
+    };
+    int arg_count = 0;
 
-    int character;
-    while ((character = fgetc(src)) != EOF) {
-        switch (character) {
-            case ' ': [[fallthrough]];
-            case '\t': [[fallthrough]];
-            case '\n': break;
-            case '#': {
-                while ((character = fgetc(src)) != '\n') {
-                    if (character == EOF)
-                        goto COMPLETE_PARSING;
+    while (true) {
+        int character = fgetc(src);
+
+        if (character == '#')
+            parse_data.state = ParseComment;
+
+        switch (parse_data.state) {
+            case ParseComment: {
+                if (character == '\n')
+                    parse_data.state = ParseOperation;
+            } break;
+            case ParseOperation: {
+                if (parse_data.string_stack.head > parse_data.string_stack.data && is_whitespace(character)) {
+                    parse_data.operation = parse_operation(pop_string(&parse_data.string_stack));
+                    arg_count = 1;
+
+                    parse_data.state = ParseArgument;
                 }
+                if (!is_whitespace(character))
+                    push_string(&parse_data.string_stack, (char)character);
             } break;
-            case '(': {
-                parse_function(src, &function_stack);
-            } break;
-            case '0': [[fallthrough]];
-            case '1': [[fallthrough]];
-            case '2': [[fallthrough]];
-            case '3': [[fallthrough]];
-            case '4': [[fallthrough]];
-            case '5': [[fallthrough]];
-            case '6': [[fallthrough]];
-            case '7': [[fallthrough]];
-            case '8': [[fallthrough]];
-            case '9': {
-                fseek(src, -1, SEEK_CUR);
-                push_variable(&variable_stack, parse_literal(src, &literal_stack));
-            } break;
-            case ')': {
-                execute_funtion(&function_stack, &variable_stack);
-            } break;
-            default:
-                ERRORF("Unexpected character '%c' to begin argument", character);
-        }
-    }
+            case ParseArgument: {
+                if (parse_data.string_stack.head > parse_data.string_stack.data && is_whitespace(character)) {
+                    --arg_count;
+                    if (arg_count == 0) {
+                        execute_operation(&parse_data, pop_string(&parse_data.string_stack));
 
-COMPLETE_PARSING:
-    printf("Result: %d\n", pop_variable(&variable_stack));
-    return;
+                        parse_data.state = ParseOperation;
+                    }
+                }
+                if (!is_whitespace(character))
+                    push_string(&parse_data.string_stack, (char)character);
+            } break;
+        }
+
+        if (character == EOF)
+            return;
+    }
 }
 
 int main(int argc, char **argv) {
     if (argc == 2) {
-        init_heap(HeapSize, StackSize);
+        constexpr usize GlobalHeapSize = 256 * 1024 * 1024;
+        constexpr usize GlobalStackSize = 16 * 1024 * 1024;
+        init_heap(GlobalHeapSize, GlobalStackSize);
 
         const char *source_name = argv[1];
         LOGF("Compiling %s", source_name);
@@ -229,11 +224,9 @@ int main(int argc, char **argv) {
             ERRORF("Could not open %s\n", source_name);
             return EXIT_FAILURE;
         }
-
         parse(src_file);
 
         LOGF("Successfully compiled %s", source_name);
-
     } else {
         printf("Usage: %s source\n", argv[0]);
         return EXIT_FAILURE;
